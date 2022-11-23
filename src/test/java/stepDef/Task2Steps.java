3e91c89ec4d15871533480265d55de881ae75402
Kiran Raju Vysyaraju <kiranrajuv@yahoo.com>
2022-11-23 12:27:01 +0530

    Task 2

--- src/test/java/stepDef/Task2Steps.java
+++ src/test/java/stepDef/Task2Steps.java
@@ -1,7 +1,6 @@
 package stepDef;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.testng.Assert;
@@ -14,45 +13,14 @@ import io.restassured.response.Response;
 
 import static org.hamcrest.Matchers.*;
 
-import schemas.Identity;
 import utils.PropertiesUtil;
 import static utils.APIUtils.*;
 
-public class APISteps {
+public class Task2Steps {
 	PropertiesUtil props = new PropertiesUtil();
 
 	private Response response;
-	private HashMap<String, Integer> currencies = new HashMap<String, Integer>();
-	
 	private String id = null;
-
-	@Given("I retrieve the IDs for the symbols")
-	public void retrieve_ids(DataTable dataTable) {
-		List<String> symbols = dataTable.asList();
-		response = doGet(props.getMapApi());
-		Identity[] identities = response.jsonPath().getObject("data", Identity[].class);
-		for (Identity identity : identities) {
-			if (symbols.contains(identity.symbol)) {
-				// System.out.println("symbol: " + identity.symbol);
-				// System.out.println("id: " + identity.id);
-				currencies.put(identity.symbol, identity.id);
-			}
-		}
-		// System.out.println(currencies);
-	}
-
-	@Then("I convert with {string} currency")
-	public void price_conversion(String currency) {
-		Map<String, String> queryParams = new HashMap<String, String>();
-		currencies.forEach((key, val) -> {
-			queryParams.put("id", val + "");
-			queryParams.put("convert", currency);
-			queryParams.put("amount", "1");
-			response = doGet(props.getPriceConvApi(), queryParams);
-			System.out.println(
-					key + "\t|\t" + val + "\t|\t" + response.jsonPath().getString("data.quote." + currency + ".price"));
-		});
-	}
 	
 	@Given("I retrieve the data for the id {string}")
 	public void retrieve_data_for_id(String id) {
--- target/test-classes/Task1.feature
+++
@@ -1,8 +0,0 @@
-Feature: Price Conversion
-
-  Scenario: Retrieve IDs and do Price Conversion
-    Given I retrieve the IDs for the symbols
-      | BTC  |
-      | USDT |
-      | ETH  |
-    Then I convert with "BOB" currency
